0e34526ef634361dd556a0b75a610f6b7de5050e
kirchma <mkirch@web.de>
2023-03-06 12:07:14 +0100

    Add stepwise charts

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
+import os
+import glob
 import pandas as pd
 from measurement import Measurement, MeasurementReaktor
+
+import settings
+settings.init()
 
 pd.set_option('display.width', 400)
 pd.set_option('display.max_columns', 10)
 
 
-path = '/Users/mkirch/OneDrive/Promotion/PERM/raw_data/HY_Z21.txt'
-measurement = Measurement(path=path)
-measurement.calculate_permeability([1e-20, 0.001], parameter='both')
-#measurement.calculate_uncertainty([1e-18, 0.001])
-#measurement.richardson_extrapolation([1e-18, 0.001])
+#path = '/Users/kirch/code/raw_data/casing2co2_part3.txt'
+#measurement = Measurement(path=path)
+#measurement.calculate_permeability([1e-19, 0.1], parameter='k')
+#measurement.richardson_extrapolation([5.678080e-19, 0.0001])
 
-#path = '/Users/mkirch/OneDrive/Promotion/PERM/raw_data/HY_RV5_05.txt'
+#path = '/Users/kirch/code/raw_data/RT_HG02_07_CO2.txt'
 #measurement = MeasurementReaktor(path=path)
 #measurement.calculate_permeability([1e-18, 0.001], parameter='both')
+
+
+#samples = ['HY_S05']
+#for i in range(len(samples)):
+#    path = os.path.join('/Users/kirch/code/raw_data/' + samples[i] + '.txt')
+#    measurement = Measurement(path=path)
+#    measurement.calculate_uncertainty()
--- plots.py
+++ plots.py
@@ -28,7 +28,7 @@ class Plotter:
             secondary_y=False)
         fig.add_trace(
             go.Scatter(x=self.df['Duration'], y=self.df['Temperature'] - 273.15, name='Temperatur',
-                       line_color='red'),
+                       line_color='red', visible='legendonly'),
             secondary_y=True)
         fig.add_vline(
             x=self.df.iloc[self.start]['Duration'], line_dash='dot')
@@ -40,7 +40,7 @@ class Plotter:
             xaxis=dict(showexponent='all', exponentformat='power'))
         fig.update_xaxes(title_text='Messdauer in s', type='log')
         fig.update_yaxes(title_text='Druck in MPa', secondary_y=False)
-        fig.update_yaxes(title_text='Temperatur in °C', secondary_y=True, range=[12, 16], showgrid=False)
+        fig.update_yaxes(title_text='Temperatur in °C', secondary_y=True, showgrid=False)
         fig.show()
 
     def result_chart(self):
@@ -86,6 +86,38 @@ class Plotter:
         fig.update_xaxes(title_text='Messdauer in s', type='log')
         fig.update_yaxes(title_text='Druck in MPa', secondary_y=False)
         fig.update_yaxes(secondary_y=True, showgrid=False)
+        fig.show()
+
+    def result_chart_stepwise(self):
+        fig = make_subplots(specs=[[{'secondary_y': True}]])
+        # Add traces
+        fig.add_trace(
+            go.Scatter(x=self.df[0]['Duration'], y=self.df[0]['Inlet_Pressure'] * 1e-6, name='Messwert Eingang',
+                       line_color='blue'))
+        fig.add_trace(
+            go.Scatter(x=self.df[0]['Duration'], y=self.df[0]['Outlet_Pressure'] * 1e-6, name='Messwert Ausgang',
+                       line_color='blue'))
+        fig.add_trace(
+            go.Scatter(x=self.df[0]['Duration'], y=self.df[0]['Inlet_Pressure_Cal'] * 1e-6, name='Rechenwert Eingang',
+                       line_color='red', mode='markers', marker=dict(size=3)))
+        fig.add_trace(
+            go.Scatter(x=self.df[0]['Duration'], y=self.df[0]['Outlet_Pressure_Cal'] * 1e-6, name='Rechenwert Ausgang',
+                       line_color='red', mode='markers', marker=dict(size=3)))
+
+        fig.add_trace(
+            go.Scatter(x=self.df[0]['Duration'], y=self.df[0]['Confining_Pressure'] * 1e-6, name='Manteldruck',
+                       line_color='black', visible='legendonly'))
+
+        for i in range(1, len(self.df)):
+            fig.add_trace(
+                go.Scatter(x=[self.df[i].iloc[-1]['Duration']], y=[self.result[i].x[0]], name='k' + str(i), line_color='black',
+                           mode='markers', marker=dict(size=10)), secondary_y=True)
+
+        fig.update_layout(
+            xaxis=dict(showexponent='all', exponentformat='power'))
+        fig.update_xaxes(title_text='Messdauer in s', type='log')
+        fig.update_yaxes(title_text='Druck in MPa', secondary_y=False)
+        fig.update_yaxes(title_text='Permeabilität in m²',secondary_y=True, showgrid=False)
         fig.show()
 
 
